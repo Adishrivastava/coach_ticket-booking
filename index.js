a3922740243b8af1304957835a4911b4c9542f4d
_ilahi <adi.shrivastava2905@gmail.com>
2021-10-29 22:24:19 +0530

    some msg

--- index.js
+++ index.js
@@ -32,13 +32,15 @@ app.use(morgan('combined'));
 
 app.use(version, routes);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-	const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// if (process.env.NODE_ENV === 'production') {
+const path = require('path');
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+// }
 
 // listening to the main port
 const server = app.listen(port, () => {
